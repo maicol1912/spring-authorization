347a46a55da1146cc8ee64bd0a0f5d33fc4c96e1
Maicol Arcila <104599986+maicol1912@users.noreply.github.com>
2023-05-17 19:17:49 -0500

    Update AccountController.java

--- src/main/java/com/spring/jwt/Jwtspring/controller/AccountController.java
+++ src/main/java/com/spring/jwt/Jwtspring/controller/AccountController.java
@@ -1,14 +1,27 @@
 package com.spring.jwt.Jwtspring.controller;
 
+import com.spring.jwt.Jwtspring.dto.LoginRequest;
 import com.spring.jwt.Jwtspring.dto.UserDTO;
+import com.spring.jwt.Jwtspring.jwt.JwtService;
 import com.spring.jwt.Jwtspring.service.UserService;
+import com.spring.jwt.Jwtspring.service.security.UserDetailCustom;
 import com.spring.jwt.Jwtspring.utilities.BaseResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationProvider;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/account")
@@ -16,9 +29,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final UserService userService;
+    private final AuthenticationProvider authenticationProvider;
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
 
-     @PostMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity<BaseResponseDTO>register(@RequestBody UserDTO userDTO){
          return ResponseEntity.ok(userService.registerAccount(userDTO));
      }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody LoginRequest request){
+        authenticateUser(request.getUsername(), request.getPassword());
+        UserDetailCustom userDetails = (UserDetailCustom) userDetailsService.loadUserByUsername(request.getUsername());
+        String token = jwtService.generateToken(userDetails);
+        Map response =  new HashMap();
+        response.put("jwt",token);
+        return ResponseEntity.ok(response);
+    }
+
+    private void authenticateUser(String username, String password) {
+        authenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
 }
